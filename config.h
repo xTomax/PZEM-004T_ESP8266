ca9ccb851f1839df9b6aaf5bc1c54ae62fd18676
Tomax@home <energico76@hotmail.com>
2022-06-24 10:42:49 +0100

    Added default config file. Needs to be filled according to user internla configs.

---
+++ config.h
@@ -0,0 +1,12 @@
+/****** Wifi Data ********/
+#define ssid ""         //Wireless SSID
+#define password ""     //Wireless Password
+
+/****** InfluxDB ********/
+#define INFLUXDB_TOKEN ""   //InfluxBD token to access deliver data.
+// InfluxDB v2 organization id (Use: InfluxDB UI -> User -> About -> Common Ids )
+#define INFLUXDB_ORG ""     //InfluxDB Organization
+#define INFLUXDB_URL ""     //InfluxDB URL Connection
+#define INFLUXDB_BUCKET ""  //InfluxDB bucket/database
+
+#define TZ_INFO "WET0WEST,M3.5.0/1,M10.5.0" //Timezone configuration
